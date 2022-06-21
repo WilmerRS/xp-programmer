4cd556b4c825a6b55e59ecbdcd4f5acbdd9c634c
FabianVC <fabianvera728@gmail.com>
2022-06-20 19:19:15 -0500

    [fix] Text rules of eslint

--- src/components/shared/text/GradientText.tsx
+++ src/components/shared/text/GradientText.tsx
@@ -1,24 +1,19 @@
 import { ReactNode } from 'react'
 
-const GradientText = ({ children }: {children: ReactNode}) => {
+const GradientText = ({ children }: { children: ReactNode }) => {
   return (
-    <span
-      className='text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-primary-600 '
-    >
+    <span className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-primary-600 ">
       {children}
-      </span>
+    </span>
   )
 }
 
-export const GradientTextDefault = ({ children }: {children: ReactNode}) => {
+export const GradientTextDefault = ({ children }: { children: ReactNode }) => {
   return (
-    <span
-      className='font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-primary-600 '
-    >
+    <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-primary-600 ">
       {children}
-      </span>
+    </span>
   )
 }
-
 
 export default GradientText
